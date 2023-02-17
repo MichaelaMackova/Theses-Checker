38f39f78690fb4b1530711594381fee48804165d
Michaela Macková <michaela.mackovaa@gmail.com>
2023-02-17 21:26:02 +0100

    File type check - JS

--- src/web/static/js/script.js
+++ src/web/static/js/script.js
@@ -1,3 +1,12 @@
 function loader() {
     document.getElementById("loader").style.display = "flex";
   }
+
+function validatePdf(fileElement) {
+  var fileName = fileElement.value;
+  var fileType = fileName.substr(fileName.lastIndexOf("."), fileName.length).toLowerCase();
+  if (fileType != ".pdf") {
+    alert("This is not pdf file!");
+    fileElement.value = "";
+  }
+}
