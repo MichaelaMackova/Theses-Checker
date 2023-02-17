76128a85260e4c917f3f184fd87b06ba9b1a2b2c
Michaela Macková <michaela.mackovaa@gmail.com>
2023-02-17 02:08:58 +0100

    check existing pdfname

--- src/web/theses_checker/views.py
+++ src/web/theses_checker/views.py
@@ -7,28 +7,40 @@ from django.conf import settings
 
 
 from .bl.theses_checker import Checker
+from .bl import auxiliary_functions
 import os
-import uuid
+
+
+
 
 
 def index(request):
     return render(request, 'theses_checker/index.html', {'text': "Hello world! This is my bachelors theses"})
 
+
+
 def checkPDF(request):
     original_pdf_path = default_storage.save(os.path.join(settings.BASE_DIR, 'files', request.FILES['file'].name), request.FILES['file'])
-    pdf_name = os.path.basename(original_pdf_path)[:-4] + '-' + uuid.uuid4().hex + '.pdf'
+    pdf_name = os.path.basename(original_pdf_path)[:-4]
+    pdf_dir = os.path.join(settings.BASE_DIR, 'static')
+
+    pdf_name = auxiliary_functions.generateUniqueFileName(pdf_dir,pdf_name,'pdf')
 
     checker = Checker(original_pdf_path)
-    checker.annotate(os.path.join(settings.BASE_DIR, 'static/', pdf_name))
+    checker.annotate(os.path.join(pdf_dir, pdf_name))
     del checker
     os.remove(original_pdf_path)
     return HttpResponseRedirect(reverse('show_annotated', args={pdf_name}))
+
+
 
 def show_annotated(request, pdf_name):
     return render(request, 'theses_checker/annotated.html', {
         'text': "Hello world! This is my bachelors theses\nThe pdf name is: " + pdf_name,
         'pdf_name': pdf_name
     })
+
+
 
 @xframe_options_exempt
 def view_annotated(request, pdf_name):
