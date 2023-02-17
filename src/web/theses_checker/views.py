4b4013bd423dc9ee843e4ceafd2f5261b8b3dd41
Michaela Macková <michaela.mackovaa@gmail.com>
2023-02-17 01:07:52 +0100

    uuid

--- src/web/theses_checker/views.py
+++ src/web/theses_checker/views.py
@@ -2,12 +2,13 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
 from django.views.decorators.clickjacking import xframe_options_exempt
+from django.core.files.storage import default_storage
+from django.conf import settings
 
 
 from .bl.theses_checker import Checker
-from django.core.files.storage import default_storage
 import os
-from django.conf import settings
+import uuid
 
 
 def index(request):
@@ -15,7 +16,7 @@ def index(request):
 
 def checkPDF(request):
     original_pdf_path = default_storage.save(os.path.join(settings.BASE_DIR, 'files', request.FILES['file'].name), request.FILES['file'])
-    pdf_name = os.path.basename(original_pdf_path)
+    pdf_name = os.path.basename(original_pdf_path)[:-4] + '-' + uuid.uuid4().hex + '.pdf'
 
     checker = Checker(original_pdf_path)
     checker.annotate(os.path.join(settings.BASE_DIR, 'static/', pdf_name))
