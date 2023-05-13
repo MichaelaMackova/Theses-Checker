0bfde9708b8aa65ca0b2ecb96a7704ab0147afdc
Michaela Macková <michaela.mackovaa@gmail.com>
2023-05-13 03:21:44 +0200

    All testing done + most TODOs

--- theses/code_examples/python_code/overflow.py
+++ theses/code_examples/python_code/overflow.py
@@ -3,9 +3,18 @@ def getOverflow(currPage):
     pageBorder = getPageBorder()
     pagePicture = getPageAsPicture(currPage)
     for lineOfPixels in pagePicture:
-        for pixel in lineOfPixels:
-            if (outOfBounds(pixel.coordinates, pageBorder)
-                    and (pixel.color != currPage.baseColor)):
-                overflowList.add(pixel)
+        for i in range(0, len(lineOfPixels)):
+            pixel = lineOfPixels[i]
+            if (not outOfBounds(pixel.coordinates, pageBorder)):
+                break
+            if (pixel.color != currPage.baseColor):
+                overflowList.add((pixel.x,pixel.y,pageBorder.x0,pixel.y))
+
+        for i in range(len(lineOfPixels), 0, -1):
+            pixel = lineOfPixels[i]
+            if (not outOfBounds(pixel.coordinates, pageBorder)):
+                break
+            if (pixel.color != currPage.baseColor):
+                overflowList.add((pageBorder.x1,pixel.y,pixel.x,pixel.y))
 
     return overflowList
